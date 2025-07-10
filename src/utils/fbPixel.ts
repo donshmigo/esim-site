e3d452e65f3cdd5c97f1439477a79468f8220a5f
donshmigo <branden.moio3@gmail.com>
2025-07-09 20:55:10 -0400

    fix: use direct fbq calls instead of react-facebook-pixel package

--- src/utils/fbPixel.ts
+++ src/utils/fbPixel.ts
@@ -1,35 +1,39 @@
-import ReactPixel from 'react-facebook-pixel';
-
-const options = {
-  autoConfig: false,
-  debug: false
-};
-
-ReactPixel.init('1727807848112777', undefined, options);
+// Direct Facebook Pixel tracking
+declare global {
+  interface Window {
+    fbq: any;
+  }
+}
 
 export const trackAddToCart = (planName: string, price: number) => {
-  ReactPixel.track('AddToCart', {
-    content_name: planName,
-    content_type: 'product',
-    value: price,
-    currency: 'USD'
-  });
+  if (typeof window !== 'undefined' && window.fbq) {
+    window.fbq('track', 'AddToCart', {
+      content_name: planName,
+      content_type: 'product',
+      value: price,
+      currency: 'USD'
+    });
+  }
 };
 
 export const trackInitiateCheckout = (planName: string, price: number) => {
-  ReactPixel.track('InitiateCheckout', {
-    content_name: planName,
-    content_type: 'product',
-    value: price,
-    currency: 'USD'
-  });
+  if (typeof window !== 'undefined' && window.fbq) {
+    window.fbq('track', 'InitiateCheckout', {
+      content_name: planName,
+      content_type: 'product',
+      value: price,
+      currency: 'USD'
+    });
+  }
 };
 
 export const trackViewContent = (planName: string, price: number) => {
-  ReactPixel.track('ViewContent', {
-    content_name: planName,
-    content_type: 'product',
-    value: price,
-    currency: 'USD'
-  });
+  if (typeof window !== 'undefined' && window.fbq) {
+    window.fbq('track', 'ViewContent', {
+      content_name: planName,
+      content_type: 'product',
+      value: price,
+      currency: 'USD'
+    });
+  }
 }; 
